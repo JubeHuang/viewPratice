4c9af6cb9459b2abf5cb7d529268c524b920a557
Jube Huang <108262303+JubeHuang@users.noreply.github.com>
2022-12-22 16:52:40 +0800

    Initial Commit

---
+++ viewPratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewPratice
+//
+//  Created by Jube on 2022/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
